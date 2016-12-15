d3952440a8bc7af47c110af011f620e68b1ef038
ntzyz <ntzyz@live.cn>
2016-12-15 12:14:36 +0800

    Init commit.

---
+++ README.md
@@ -0,0 +1,56 @@
+Portal
+======
+
+TODO: 
+
+### HTML Example
+```html
+<h1>Telnet 论坛</h1>
+<p>
+  本站现已支持使用Telnet访问！网址同主站网址，为 www.cnvintage.org，端口为 50000。
+  当前您的访问Token：<span id="telnet-token">未刷新</span>
+</p>
+<button type="submit" class="Button" onclick="flushToken()">
+  <span class="Button-label">刷新Token</span>
+</button>
+<script>
+function flushToken() {
+  // New Ajax request.
+  var xhr = new XMLHttpRequest();
+
+  // Open it.
+  xhr.open('GET', '/portal.php');
+
+  // Setup event listener.
+  xhr.onreadystatechange = function() {
+    switch(xhr.readyState) {
+      case XMLHttpRequest.DONE: if (xhr.status === 200) {
+        var res = JSON.parse(xhr.responseText);
+        if (res.status === 'success') {
+          document.querySelector('#telnet-token').innerText = res.token;
+        } else {
+          document.querySelector('#telnet-token').innerText = '出现错误：' + res.message;
+        }
+      }
+      break;
+      case XMLHttpRequest.LOADING:
+        document.querySelector('#telnet-token').innerText = '正在获取...';
+      default: break;
+    }
+  };
+
+  // Let it go.
+  xhr.send();
+}
+</script>
+```
+
+### Database
+```sql
+CREATE TABLE `fl_telnet_access_tokens` (
+  `remote_addr` varchar(50) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `access_token` varchar(10) NOT NULL,
+  `flarum_token` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+```
---
+++ portal.php
@@ -0,0 +1,64 @@
+<?php
+  function generateRandomString($length = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+      $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+  }
+  header('Content-Type: application/json');
+  header("Access-Control-Allow-Origin: *");
+
+  // Load flarum config.
+  $config = include('config.php');
+  // Fetch remote IP address
+  $remoteIP = $_SERVER['REMOTE_ADDR'];
+  // unused
+  $operation = isset($_GET['op']) ? $_GET['op'] : 'request';
+  // Response object.
+  $res = (object)[]; 
+  // 
+  $access_token = null;
+  
+  // Connect to MySQL Server with mysqli
+  $mysqli = new mysqli($config['database']['host'], 
+                       $config['database']['username'], 
+                       $config['database']['password'],
+                       $config['database']['database']);
+  // Check connection
+  if ($mysqli->connect_errno) {
+    $res->status = 'error';
+    $res->message = 'Failed to connect to MySQL: ' . $mysqli->connect_error;
+    die(json_encode($res));
+  }
+
+  // Fetch the cookie.
+  if (isset($_COOKIE['flarum_remember'])) {
+    $access_token = $_COOKIE['flarum_remember'];
+  }
+  else {
+    $res->status = 'error';
+    $res->message = 'Unauthorized access.';
+    die(json_encode($res));
+  }
+
+  // Get user informathion form cookie.
+  $access_token = $mysqli->real_escape_string($access_token);
+  $result = $mysqli->query("SELECT user_id FROM fl_access_tokens WHERE fl_access_tokens.id = '$access_token'");
+  $row = $result->fetch_assoc();
+  $user_id = $row['user_id'];
+
+  // Remove all old tokens
+  $mysqli->query("DELETE FROM fl_telnet_access_tokens WHERE user_id = $user_id");
+
+  // Insert new tokens
+  $new_token = generateRandomString(5);
+  $mysqli->query("INSERT INTO fl_telnet_access_tokens VALUES('$remoteIP', $user_id, '$new_token')");
+  
+  // Create a new token and insert into database
+  $res->status = 'success';
+  $res->token = $new_token;
+  echo(json_encode($res));
+  
